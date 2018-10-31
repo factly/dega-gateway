898a5dda381ede6c65d0b94038d221937e1d9c42
Shashi Deshetti <deshetti@gmail.com>
2018-10-30 19:09:36 -0500

    Added entity: Status

---
+++ src/test/javascript/e2e/entities/core/status/status.page-object.ts
@@ -0,0 +1,76 @@
+import { element, by, ElementFinder } from 'protractor';
+
+export class StatusComponentsPage {
+    createButton = element(by.id('jh-create-entity'));
+    deleteButtons = element.all(by.css('jhi-status div table .btn-danger'));
+    title = element.all(by.css('jhi-status div h2#page-heading span')).first();
+
+    async clickOnCreateButton() {
+        await this.createButton.click();
+    }
+
+    async clickOnLastDeleteButton() {
+        await this.deleteButtons.last().click();
+    }
+
+    async countDeleteButtons() {
+        return this.deleteButtons.count();
+    }
+
+    async getTitle() {
+        return this.title.getAttribute('jhiTranslate');
+    }
+}
+
+export class StatusUpdatePage {
+    pageTitle = element(by.id('jhi-status-heading'));
+    saveButton = element(by.id('save-entity'));
+    cancelButton = element(by.id('cancel-save'));
+    nameInput = element(by.id('field_name'));
+    clientIdInput = element(by.id('field_clientId'));
+
+    async getPageTitle() {
+        return this.pageTitle.getAttribute('jhiTranslate');
+    }
+
+    async setNameInput(name) {
+        await this.nameInput.sendKeys(name);
+    }
+
+    async getNameInput() {
+        return this.nameInput.getAttribute('value');
+    }
+
+    async setClientIdInput(clientId) {
+        await this.clientIdInput.sendKeys(clientId);
+    }
+
+    async getClientIdInput() {
+        return this.clientIdInput.getAttribute('value');
+    }
+
+    async save() {
+        await this.saveButton.click();
+    }
+
+    async cancel() {
+        await this.cancelButton.click();
+    }
+
+    getSaveButton(): ElementFinder {
+        return this.saveButton;
+    }
+}
+
+export class StatusDeleteDialog {
+    private dialogTitle = element(by.id('jhi-delete-status-heading'));
+    private confirmButton = element(by.id('jhi-confirm-delete-status'));
+
+    async getDialogTitle() {
+        return this.dialogTitle.getAttribute('jhiTranslate');
+    }
+
+    async clickOnConfirmButton() {
+        await this.confirmButton.click();
+    }
+}

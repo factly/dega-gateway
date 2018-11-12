047d97d3b6f5993b07b075788e593893d0d01af9
Ramesh-ns <ramesh.yenugula@gmail.com>
2018-11-12 12:42:54 -0700

    Changes for Quill editor

--- src/main/webapp/app/entities/core/post/post.module.ts
+++ src/main/webapp/app/entities/core/post/post.module.ts
@@ -2,6 +2,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
+import { QuillModule } from 'ngx-quill';
+
 import {
     PostComponent,
     PostDetailComponent,
@@ -15,7 +17,7 @@ import {
 const ENTITY_STATES = [...postRoute, ...postPopupRoute];
 
 @NgModule({
-    imports: [GatewaySharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [GatewaySharedModule, QuillModule, RouterModule.forChild(ENTITY_STATES)],
     declarations: [PostComponent, PostDetailComponent, PostUpdateComponent, PostDeleteDialogComponent, PostDeletePopupComponent],
     entryComponents: [PostComponent, PostUpdateComponent, PostDeleteDialogComponent, PostDeletePopupComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]

b15e8f4d53ca7f7d6af8f90b6e0203e8cbac3638
Ramesh-ns <ramesh.yenugula@gmail.com>
2018-12-02 14:07:31 -0700

    Changes for creating slug for status by name

--- src/main/webapp/app/entities/core/status/status-update.component.ts
+++ src/main/webapp/app/entities/core/status/status-update.component.ts
@@ -17,6 +17,9 @@ export class StatusUpdateComponent implements OnInit {
     isSaving: boolean;
     createdDate: string;
     lastUpdatedDate: string;
+    slug: string;
+    slugExtention: number;
+    tempSlug: string;
 
     constructor(private statusService: StatusService, private activatedRoute: ActivatedRoute) {}
 
@@ -55,5 +58,24 @@ export class StatusUpdateComponent implements OnInit {
 
     private onSaveError() {
         this.isSaving = false;
+    }
+
+    bindSlug(event: any) {
+        this.slugExtention = 0;
+        this.slug = event.target.value.replace(/[^\w\s]/gi, '').replace(/\s+/g, '-');
+        this.tempSlug = this.slug;
+        this.createSlug();
+    }
+    createSlug() {
+        if (this.slug) {
+            this.statusService.getStatusBySlug(this.slug).subscribe((res: HttpResponse<IStatus>) => {
+                if (res.body) {
+                    this.slugExtention += 1;
+                    this.slug = this.tempSlug + this.slugExtention;
+                    this.createSlug();
+                }
+                this.status.slug = this.slug;
+            });
+        }
     }
 }

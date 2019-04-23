1b784a950b1732060bbef26703376383defab169
Arghya Saha <arghyasaha26@gmail.com>
2019-04-23 23:22:34 +0530

    fixed the admin load issue

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material';
 import { MatDialogModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { QuillModule } from 'ngx-quill';
@@ -13,7 +12,7 @@ import { NgbDateMomentAdapter } from 'app/shared/util/datepicker-adapter';
 import { GatewaySharedCommonModule, GatewaySharedLibsModule, HasAnyAuthorityDirective } from 'app/shared/index';
 
 @NgModule({
-    imports: [BrowserAnimationsModule, GatewaySharedLibsModule, GatewaySharedCommonModule, MatCheckboxModule, MatDialogModule, QuillModule],
+    imports: [GatewaySharedLibsModule, GatewaySharedCommonModule, MatCheckboxModule, MatDialogModule, QuillModule],
     declarations: [HasAnyAuthorityDirective, MultipleCheckboxComponent, QuillEditorComponent, QuillEditorFileUploadComponent],
     providers: [
         {

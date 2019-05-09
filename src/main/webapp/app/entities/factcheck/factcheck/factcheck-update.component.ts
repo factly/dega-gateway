81ed49bae723fa2efe0290b9544f82496a961208
Arghya Saha <arghyasaha26@gmail.com>
2019-05-09 22:39:06 +0530

    fixes #158 issue

--- src/main/webapp/app/entities/factcheck/factcheck/factcheck-update.component.ts
+++ src/main/webapp/app/entities/factcheck/factcheck/factcheck-update.component.ts
@@ -224,8 +224,8 @@ export class FactcheckUpdateComponent implements OnInit {
 
     saveOrPublish(statusName) {
         this.isSaving = true;
+        this.factcheck.statusName = statusName;
         if (this.factcheck.id !== undefined) {
-            this.factcheck.statusName = statusName;
             this.subscribeToSaveResponse(this.factcheckService.update(this.factcheck));
         } else {
             this.subscribeToSaveResponse(this.factcheckService.create(this.factcheck));

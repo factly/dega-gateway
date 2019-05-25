c1fb69adc1421cad0be5f1ab5fc2360ad6e1dced
Shashi Kiran Deshetti <deshetti@Shashis-MBP.lan>
2019-05-25 12:17:27 -0500

    added changes needed to create a new profile

--- src/main/java/com/factly/dega/config/OAuth2Configuration.java
+++ src/main/java/com/factly/dega/config/OAuth2Configuration.java
@@ -31,7 +31,7 @@ import java.io.IOException;
  * mode too.
  */
 @Configuration
-@Profile("dev")
+@Profile({"dev","stag"})
 public class OAuth2Configuration {
     public static final String SAVED_LOGIN_ORIGIN_URI = OAuth2Configuration.class.getName() + "_SAVED_ORIGIN";
 

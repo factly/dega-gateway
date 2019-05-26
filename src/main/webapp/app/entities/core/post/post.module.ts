a6cfc7a19d096c5378704c32daac053e348a7bf1
Arghya Saha <arghyasaha26@gmail.com>
2019-05-26 21:58:43 +0530

    added material import

--- src/main/webapp/app/entities/core/post/post.module.ts
+++ src/main/webapp/app/entities/core/post/post.module.ts
@@ -1,5 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
@@ -20,7 +26,16 @@ import {
 const ENTITY_STATES = [...postRoute, ...postPopupRoute];
 
 @NgModule({
-    imports: [GatewaySharedModule, RouterModule.forChild(ENTITY_STATES), MatTooltipModule],
+    imports: [
+        GatewaySharedModule,
+        RouterModule.forChild(ENTITY_STATES),
+        MatButtonModule,
+        MatCheckboxModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTooltipModule
+    ],
     declarations: [
         PostComponent,
         PostDetailComponent,

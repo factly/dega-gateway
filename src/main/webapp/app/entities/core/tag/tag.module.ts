b6757067b28c2126d8179a18888dd93351af3486
Prasoon Kumar <prasoon.kumar@quikr.com>
2019-06-01 17:11:30 +0530

    Tag list and edit view UI changes

--- src/main/webapp/app/entities/core/tag/tag.module.ts
+++ src/main/webapp/app/entities/core/tag/tag.module.ts
@@ -2,6 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
 import {
     TagComponent,
     TagDetailComponent,
@@ -15,7 +21,7 @@ import {
 const ENTITY_STATES = [...tagRoute, ...tagPopupRoute];
 
 @NgModule({
-    imports: [GatewaySharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [GatewaySharedModule, MatButtonModule, MatIconModule, MatInputModule, MatTableModule, RouterModule.forChild(ENTITY_STATES)],
     declarations: [TagComponent, TagDetailComponent, TagUpdateComponent, TagDeleteDialogComponent, TagDeletePopupComponent],
     entryComponents: [TagComponent, TagUpdateComponent, TagDeleteDialogComponent, TagDeletePopupComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]

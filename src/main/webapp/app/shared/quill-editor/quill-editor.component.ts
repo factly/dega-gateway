533ee5558bb6dbe9a6a2f0f551c9f8f70854be79
Arghya Saha <arghyasaha26@gmail.com>
2019-06-06 01:05:18 +0530

    div element wrapper over iframe added

--- src/main/webapp/app/shared/quill-editor/quill-editor.component.ts
+++ src/main/webapp/app/shared/quill-editor/quill-editor.component.ts
@@ -1,8 +1,35 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 import { MatDialog } from '@angular/material';
 
 import { QuillEditorFileUploadComponent } from 'app/shared/quill-editor/quill-editor-file-upload.component';
+import * as Quill from 'quill';
+const BlockEmbed = Quill.import('blots/block');
+
+export class Video extends BlockEmbed {
+    static create(value) {
+        let node = super.create(value);
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowfullscreen', 'true');
+        iframe.setAttribute('src', value);
+
+        iframe.className = 'test-class';
+        node.appendChild(iframe);
+        console.log('called');
+        return node;
+    }
+
+    static value(domNode) {
+        return domNode.firstChild.getAttribute('src');
+    }
+}
+
+Video['blotName'] = 'video';
+Video['className'] = 'ql-video';
+Video['tagName'] = 'div';
+
+Quill.register('formats/video', Video);
 
 @Component({
     selector: 'jhi-quill-editor',
@@ -22,7 +49,11 @@ export class QuillEditorComponent {
     modules = {
         table: true
     };
-
+    test_content = `<div class="fluid-width-video-wrapper">
+   <div class="ql-video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/QpvEWVVnICE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="position:absolute; left:0px; top:0px; width:100%; height:100%;"></iframe>
+   </div>
+</div>`;
     constructor(private dialog: MatDialog) {}
 
     emitEventOnTextChange({ quill, html, text }) {
@@ -34,6 +65,7 @@ export class QuillEditorComponent {
 
     getEditorInstance(editorInstance: any) {
         this.quillEditorRef = editorInstance;
+        // editorInstance.register(Video)
         const toolbar = editorInstance.getModule('toolbar');
         this.tableInstance = editorInstance.getModule('table');
         toolbar.addHandler('image', this.openFileUploadDialog.bind(this));
@@ -47,8 +79,7 @@ export class QuillEditorComponent {
         const config = {
             height: '90%',
             width: '90vw',
-            maxWidth: '90vw',
-            data: { url: 'https://cdn.pixabay.com/photo/2016/05/17/22/16/baby-1399332_1280.jpg' }
+            maxWidth: '90vw'
         };
         const dialogRef = this.dialog.open(QuillEditorFileUploadComponent, config);
 
@@ -90,3 +121,12 @@ export class QuillEditorComponent {
         this.tableInstance.deleteTable();
     }
 }
+
+// const QuillBlockEmbed = Quill.import('blots/block/embed');
+//
+//
+// class Video extends QuillBlockEmbed {
+//
+// }
+
+// Quill.register(Video);

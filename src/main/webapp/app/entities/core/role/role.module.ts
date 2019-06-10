396bd38965690f2f2a01614bf11f5f6c5935c073
Prasoon Kumar <prasoon.kumar@quikr.com>
2019-06-10 20:41:04 +0530

    Category, format, status and roles listing and edit page  ui

--- src/main/webapp/app/entities/core/role/role.module.ts
+++ src/main/webapp/app/entities/core/role/role.module.ts
@@ -2,6 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
 import {
     RoleComponent,
     RoleDetailComponent,
@@ -15,7 +23,16 @@ import {
 const ENTITY_STATES = [...roleRoute, ...rolePopupRoute];
 
 @NgModule({
-    imports: [GatewaySharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [
+        GatewaySharedModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatTableModule,
+        RouterModule.forChild(ENTITY_STATES)
+    ],
     declarations: [RoleComponent, RoleDetailComponent, RoleUpdateComponent, RoleDeleteDialogComponent, RoleDeletePopupComponent],
     entryComponents: [RoleComponent, RoleUpdateComponent, RoleDeleteDialogComponent, RoleDeletePopupComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]

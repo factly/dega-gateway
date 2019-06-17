1244160da358d006d25145e48b62d06d0c8f0aa1
Prasoon Kumar <prasoon.kumar@quikr.com>
2019-06-18 00:39:39 +0530

    Featured media pop css

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { ConfirmationDialogComponent } from 'app/shared/confirmation-dialog/conf
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
@@ -24,6 +25,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
         MatButtonModule,
         MatDialogModule,
         MatIconModule,
+        MatInputModule,
         MatTooltipModule,
         QuillModule
     ],

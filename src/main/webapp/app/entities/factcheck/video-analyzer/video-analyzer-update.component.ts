ca4327f9f96c8763262f2e570e521abefa5e35ae
Arghya Saha <arghyasaha26@gmail.com>
2019-09-29 14:01:19 +0530

    added video model

--- src/main/webapp/app/entities/factcheck/video-analyzer/video-analyzer-update.component.ts
+++ src/main/webapp/app/entities/factcheck/video-analyzer/video-analyzer-update.component.ts
@@ -1,81 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
-import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
 
 import { IRating } from 'app/shared/model/factcheck/rating.model';
 import { VideoAnalyzerService } from './video-analyzer.service';
 import { ADMIN_ROLE } from 'app/shared/constants/role.constants';
-import { DegaUserService } from 'app/entities/core/dega-user';
+
 import { IDegaUser } from 'app/shared/model/core/dega-user.model';
 import { JhiAlertService } from 'ng-jhipster';
-import { Account, Principal } from 'app/core';
 
-import { QuillEditorFileUploadComponent } from 'app/shared/quill-editor/quill-editor-file-upload.component';
+import { IVideoAnalyzer } from 'app/shared/model/factcheck/video-analyzer.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'jhi-rating-update',
     templateUrl: './video-analyzer-update.component.html'
 })
 export class VideoAnalyzerUpdateComponent implements OnInit {
-    rating: IRating;
-    degausers: IDegaUser[];
+    videoData: IVideoAnalyzer;
+    videoAnalysisData: any[];
+
     isSaving: boolean;
-    createdDate: string;
-    lastUpdatedDate: string;
-    slug: string;
-    showClientId: boolean;
-    currentUser: IDegaUser;
-    account: Account;
+
+    videoFormGroup: FormGroup;
+    videoAnalysisFormGroup: FormGroup;
 
     constructor(
         private jhiAlertService: JhiAlertService,
-        private ratingService: VideoAnalyzerService,
+        private videoAnalyzerService: VideoAnalyzerService,
         private activatedRoute: ActivatedRoute,
-        private degaUserService: DegaUserService,
-        private principal: Principal,
-        private dialog: MatDialog
+        private fb: FormBuilder
     ) {}
 
     ngOnInit() {
         this.isSaving = false;
-        this.activatedRoute.data.subscribe(({ rating }) => {
-            this.rating = rating;
-            this.createdDate = this.rating.createdDate != null ? this.rating.createdDate.format(DATE_TIME_FORMAT) : null;
-            this.lastUpdatedDate = this.rating.lastUpdatedDate != null ? this.rating.lastUpdatedDate.format(DATE_TIME_FORMAT) : null;
+        this.videoFormGroup = this.fb.group({
+            id: [this.videoData.id || ''],
+            title: [this.videoData.title || '', Validators.required],
+            description: [this.videoData.description || '', Validators.required],
+            status_id: [this.videoData.status.id || '', Validators.required]
         });
-        this.principal.identity().then(account => {
-            this.account = account;
-        });
-        this.degaUserService.query().subscribe(
-            (res: HttpResponse<IDegaUser[]>) => {
-                this.degausers = res.body;
-                this.currentUser = this.degausers.filter(u => u.email === this.account.email).shift();
-                this.showClientId = this.showClientIdField(this.currentUser.roleName);
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
     }
 
     previousState() {
         window.history.back();
     }
 
-    save() {
+    saveVideoData() {
         this.isSaving = true;
-        this.rating.createdDate = this.createdDate != null ? moment(this.createdDate, DATE_TIME_FORMAT) : null;
-        this.rating.lastUpdatedDate = this.lastUpdatedDate != null ? moment(this.lastUpdatedDate, DATE_TIME_FORMAT) : null;
-        if (this.rating.id !== undefined) {
-            this.subscribeToSaveResponse(this.ratingService.update(this.rating));
+        if (this.videoFormGroup.value.id !== undefined) {
+            this.subscribeToSaveResponse(this.videoAnalyzerService.update(this.videoFormGroup.value));
         } else {
-            this.subscribeToSaveResponse(this.ratingService.create(this.rating));
+            this.subscribeToSaveResponse(this.videoAnalyzerService.create(this.videoFormGroup.value));
         }
     }
 
-    private subscribeToSaveResponse(result: Observable<HttpResponse<IRating>>) {
+    saveVideoAnalysisData() {
+        this.isSaving = true;
+        if (this.videoFormGroup.value.id !== undefined) {
+            this.subscribeToSaveResponse(this.videoAnalyzerService.update(this.videoFormGroup.value));
+        } else {
+            this.subscribeToSaveResponse(this.videoAnalyzerService.create(this.videoFormGroup.value));
+        }
+    }
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<IVideoAnalyzer>>) {
         result.subscribe((res: HttpResponse<IRating>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
     }
 
@@ -86,25 +76,6 @@ export class VideoAnalyzerUpdateComponent implements OnInit {
 
     private onSaveError() {
         this.isSaving = false;
-    }
-
-    choseMediaforFeature() {
-        const config = {
-            height: '90%',
-            width: '90vw',
-            maxWidth: '90vw'
-        };
-        const dialogRef = this.dialog.open(QuillEditorFileUploadComponent, config);
-
-        dialogRef.afterClosed().subscribe(imageData => {
-            if (imageData) {
-                this.updateMediaForFeature(imageData);
-            }
-        });
-    }
-
-    updateMediaForFeature(imageData) {
-        this.rating.media = imageData;
     }
 
     private onError(errorMessage: string) {
--- src/main/webapp/app/shared/model/factcheck/video-analyzer.model.ts
+++ src/main/webapp/app/shared/model/factcheck/video-analyzer.model.ts
@@ -1,31 +1,27 @@
 import { Moment } from 'moment';
+import { IStatus, Status } from 'app/shared/model/core/status.model';
 
 export interface IVideoAnalyzer {
     id?: string;
-    name?: string;
-    numericValue?: number;
-    iconURL?: string;
-    isDefault?: boolean;
+    title?: string;
     clientId?: string;
     slug?: string;
     createdDate?: Moment;
     lastUpdatedDate?: Moment;
     description?: string;
+    status?: IStatus;
+    link?: string;
 }
 
 export class VideoAnalyzer implements IVideoAnalyzer {
     constructor(
         public id?: string,
-        public name?: string,
-        public numericValue?: number,
-        public iconURL?: string,
-        public isDefault?: boolean,
         public clientId?: string,
         public slug?: string,
         public createdDate?: Moment,
         public lastUpdatedDate?: Moment,
-        public description?: string
-    ) {
-        this.isDefault = this.isDefault || false;
-    }
+        public description?: string,
+        public status?: Status,
+        public link?: string
+    ) {}
 }

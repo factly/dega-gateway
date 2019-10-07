10bd2331efa17933df53b15e273600d551a90788
Rahul Jaisinghani <rahuljaisinghani1997@gmail.com>
2019-10-07 12:58:13 +0530

    removed unwanted statement

--- src/main/webapp/app/entities/core/media/media-detail.component.ts
+++ src/main/webapp/app/entities/core/media/media-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Pipe, PipeTransform } from '@angular/core';
 import { IMedia } from 'app/shared/model/core/media.model';
 
 @Component({

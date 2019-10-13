5b57e819d20c78b0173d9f29506dfcb8b23d7144
Rahul Jaisinghani <rahuljaisinghani1997@gmail.com>
2019-10-14 02:01:06 +0530

    converted model forms to reactive forms for organization, Dega User, Role Mapping

--- src/main/webapp/app/entities/core/dega-user/dega-user.module.ts
+++ src/main/webapp/app/entities/core/dega-user/dega-user.module.ts
@@ -2,6 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import {
     DegaUserComponent,
     DegaUserDetailComponent,
@@ -15,7 +22,16 @@ import {
 const ENTITY_STATES = [...degaUserRoute, ...degaUserPopupRoute];
 
 @NgModule({
-    imports: [GatewaySharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [
+        GatewaySharedModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatTableModule,
+        RouterModule.forChild(ENTITY_STATES)
+    ],
     declarations: [
         DegaUserComponent,
         DegaUserDetailComponent,

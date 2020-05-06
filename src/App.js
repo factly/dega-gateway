fcc1ca56a268f63cff7d61425881eb307a89e37b
Monark <monark@factly.in>
2020-05-06 12:17:52 +0530

    login removed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import "antd/dist/antd.css";
-import Login from "./pages/login";
-import Dashboard from "./pages/dashboard";
+import 'antd/dist/antd.css';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/auth/login" component={Login} />
         <Route path="/" component={Dashboard} />
       </Router>
     </div>
